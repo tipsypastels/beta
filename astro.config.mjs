487864d9474cdea0d75499bbfb4f281e705a7342
tipsypastels <dakotasankey@gmail.com>
2026-02-02 18:50:11 -0800

    a basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://tipsypastels.github.io",
   base: "/beta",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
