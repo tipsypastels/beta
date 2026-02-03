09549d436ca28dc4183139cc3e1e27695b66a9ba
tipsypastels <dakotasankey@gmail.com>
2026-02-03 15:55:57 -0800

    framer

--- src/components/ws/Crowd.tsx
+++ src/components/ws/Crowd.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/pro-solid-svg-icons";
 import { $connected, $username, wsOn, wsSend } from "./store";
 import { useStore } from "@nanostores/react";
+import { AnimatePresence, motion } from "motion/react";
 
 export function Crowd() {
   const connected = useStore($connected);
@@ -25,29 +26,33 @@ export function Crowd() {
     }
   }, [connected]);
 
-  if (users.length === 0) return null;
-
   return (
-    <aside>
-      <div className="inline-flex items-center rounded-2xl border-2 border-yellow-600 bg-stone-950 p-2">
-        <FontAwesomeIcon icon={faEye} className="text-yellow-600" size="2x" />
-        <ul className="ml-2 flex gap-2">
-          {users.map((user) => (
-            <li
-              key={user.uuid}
-              style={{ backgroundColor: user.color }}
-              className="flex h-8 w-8 items-center justify-center rounded-full border-yellow-600 text-xl font-bold text-black select-none data-[idle=true]:opacity-50 data-[you=true]:border-2"
-              title={`${user.username ?? "Guest"}${user.you ? " (You)" : ""}`}
-              data-you={user.you}
-              data-idle={
-                !user.you && user.active <= Date.now() - 10 * 60 * 1000
-              }
-            >
-              {user.username?.charAt(0).toUpperCase() ?? "?"}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </aside>
+    <AnimatePresence>
+      {users.length > 0 && (
+        <motion.div
+          className="inline-flex items-center rounded-2xl border-2 border-yellow-600 bg-stone-950 p-2"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <FontAwesomeIcon icon={faEye} className="text-yellow-600" size="2x" />
+          <ul className="ml-2 flex gap-2">
+            {users.map((user) => (
+              <li
+                key={user.uuid}
+                style={{ backgroundColor: user.color }}
+                className="flex h-8 w-8 items-center justify-center rounded-full border-yellow-600 text-xl font-bold text-black select-none data-[idle=true]:opacity-50 data-[you=true]:border-2"
+                title={`${user.username ?? "Guest"}${user.you ? " (You)" : ""}`}
+                data-you={user.you}
+                data-idle={
+                  !user.you && user.active <= Date.now() - 10 * 60 * 1000
+                }
+              >
+                {user.username?.charAt(0).toUpperCase() ?? "?"}
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
--- src/components/ws/Login.tsx
+++ src/components/ws/Login.tsx
@@ -1,32 +1,39 @@
 import { useStore } from "@nanostores/react";
 import { $connected, $username, wsSend } from "./store";
 import { useState } from "react";
+import { AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 
 export function Login() {
   const connected = useStore($connected);
   const username = useStore($username);
   const [usernameInput, setUsernameInput] = useState("");
 
-  if (!connected || username) return null;
-
   return (
-    <form
-      className="fixed bottom-0 left-0 my-8 flex w-screen items-center justify-center"
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (!usernameInput) return;
+    <AnimatePresence>
+      {connected && !username ? (
+        <motion.form
+          className="fixed bottom-0 left-0 my-8 flex w-screen items-center justify-center"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!usernameInput) return;
 
-        wsSend({ type: "identify", username: usernameInput });
-        $username.set(usernameInput);
-        localStorage.setItem("beta:username", usernameInput);
-      }}
-    >
-      <input
-        className="rounded-lg border-2 border-white bg-yellow-600 p-4 shadow-2xl"
-        placeholder="Enter a username?"
-        value={usernameInput}
-        onChange={(e) => setUsernameInput(e.target.value)}
-      />
-    </form>
+            wsSend({ type: "identify", username: usernameInput });
+            $username.set(usernameInput);
+            localStorage.setItem("beta:username", usernameInput);
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2 }}
+        >
+          <input
+            className="rounded-lg border-2 border-white bg-yellow-600 p-4 shadow-2xl"
+            placeholder="Enter a username?"
+            value={usernameInput}
+            onChange={(e) => setUsernameInput(e.target.value)}
+          />
+        </motion.form>
+      ) : null}
+    </AnimatePresence>
   );
 }
